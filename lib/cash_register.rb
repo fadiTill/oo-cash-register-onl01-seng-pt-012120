93bd6871f3a6c2937071d1c90ad7792e47cc4f06
fadimata tillman <fadi.tillman@gmail.com>
2020-02-13 19:45:38 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -36,7 +36,7 @@ class CashRegister
   end
 
 
-  def void_last_transaction(total=@last_transaction_amount)
+  def void_last_transaction(total-=@last_transaction_amount)
     @total -= @last_transaction_amount
   end
 end
